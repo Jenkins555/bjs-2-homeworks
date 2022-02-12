c060694a3475d9722cb36087da73f4cce24ff77c
Анисимов Александр <ujc90@mail.ru>
2022-02-12 13:13:40 +0300

    task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,20 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
-
+ for(let i = 0; i < arr1.lenght; i++){
+  result = arr1.every(item => item === arr2[i]);
+ }
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+let resultArr;
+resultArr = arr.filter(function(num) {
+ return num > 0;
+});
+resultArr = arr.filter(function(num) {
+  return num % 3 === 0;
+});
+resultArr = arr.map(num => num * 10 );
+ return resultArr; // array
 }
+
