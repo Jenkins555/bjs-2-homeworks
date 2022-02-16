1f3bf5a34719fbeffe541bc960312116dcc21282
Анисимов Александр <ujc90@mail.ru>
2022-02-16 15:54:27 +0300

    Редактирование task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,47 @@
 function Student(name, gender, age) {
-    // Ваш код
+  // Ваш код
 
 }
+let student3 = new Student("Михаил", "male", 20);
+let student4 = new Student("Виктория", "female", 19);
+
+
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
+
 }
 
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [mark]; // первая оценка
+  } else {
+    this.marks.push(mark); //последующие оценки
+  }
+}
+
+Student.prototype.addMarks = function (...mark) {
+  if (this.marks === undefined) {
+    this.marks = [...mark]; // если нет оценок
+  } else {
+    this.marks.push(...mark); //добавление оценок
+  }
+}
+
+Student.prototype.getAverage = function () {
+  let sum = 0;
+  for (let i = 0; i < this.marks.length; i++) {
+    sum = + this.marks[i];
+  }
+  return sum / this.marks.length;
+  ///среднее арифметическое оценок студента
+}
+
+Student.prototype.exclude = function (reason) {
+  delete Student['subject'];
+  delete Student['marks'];
+  Student['exclude'] = "reason";
+
+  ///Исключить студента и установить причину
+}
 // ваш код для остальных методов
